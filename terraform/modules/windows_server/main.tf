9b76d89db7b0113b7696347aff33a2a9b8b1a80d
rodmassis <rod.assis@gmail.com>
2025-06-01 22:53:54 -0300

    Tweaks

--- terraform/modules/windows_server/main.tf
+++ terraform/modules/windows_server/main.tf
@@ -21,6 +21,14 @@ resource "aws_security_group" "this" {
     cidr_blocks = var.allowed_ips
   }
 
+  ingress {
+    description = "Allow WinRM HTTPS"
+    from_port   = 5986
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # or limit to your IP
+  }
+
   # Allow internal AD traffic (Linux join, ping, etc.)
   ingress {
     from_port   = 0
@@ -62,6 +70,30 @@ resource "aws_instance" "this" {
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.this.id]
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+  <powershell>
+  # Install WinRM
+  winrm quickconfig -q
+  Enable-PSRemoting -Force
+
+  # Create Self-Signed Cert
+  $Cert = New-SelfSignedCertificate -DnsName "$env:COMPUTERNAME" -CertStoreLocation Cert:\LocalMachine\My
+
+  # Create HTTPS Listener
+  New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbprint $Cert.Thumbprint -Force
+
+  # Allow Unencrypted/BASIC Auth
+  winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+  winrm set winrm/config/service/auth '@{Basic="true"}'
+
+  # Set WinRM timeout
+  winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+
+  # Open firewall
+  New-NetFirewallRule -Name "WinRM-HTTPS" -DisplayName "WinRM over HTTPS" -Enabled True -Protocol TCP -LocalPort 5986 -Action Allow
+  </powershell>
+  EOF
 
   tags = {
     Name = "${var.project_prefix}-windows"
